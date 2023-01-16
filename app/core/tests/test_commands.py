a9181ab03382e9ba0413c1ec31f7be1ebc0e56ae
teeraphon <teeraphon.iss@gmail.com>
2023-01-16 23:50:01 +0700

    Lint3

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,10 +9,12 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
 
+
     def test_wait_for_db_ready(self, patched_check):
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
+
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -22,6 +24,4 @@ class CommandTest(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
-
-      
+        patched_check.assert_called_with(databases=['default'])     
